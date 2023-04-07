43d73f8dbbf97d2bb7ad27142c78f36517249ebe
Harshit <harshitgulati6126@gmail.com>
2023-04-07 15:21:07 +0530

    aa

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
+ // Get the button element
+const button = document.querySelector('.navbar-toggler');
+
+// Get the image container element
+const imageContainer = document.querySelector('.cap-img');
+
+// Store the original top value
+const originalTop = imageContainer.style.top;
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+  // If the current top value is the original value, increase it by 20 pixels
+  if (imageContainer.style.top === originalTop) {
+    imageContainer.style.top = `${parseInt(originalTop) + 20}px`;
+  } else { // Otherwise, revert the top value to the original value
+    imageContainer.style.top = originalTop;
+  }
+});
